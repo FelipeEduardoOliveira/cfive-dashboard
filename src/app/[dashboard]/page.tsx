a9d83a57e91232cc0ad7d83e7792ee0fbd40371f
Felipe Oliveira <felipe.oliveira@quikdev.com.br>
2025-04-21 16:41:36 -0300

    feat: resolve padding

--- src/app/[dashboard]/page.tsx
+++ src/app/[dashboard]/page.tsx
@@ -4,8 +4,12 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className=" flex justify-center items-center min-h-screen">
-      <h1>Wellcome to dashboard page</h1>
+    <div className="flex flex-col justify-between items-center min-h-screen p-2.5">
+      <div>Cabeçalho</div>
+      <div>
+        <h1>Wellcome to dashboard page</h1>
+      </div>
+      <div>Rodapé</div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import LoginForm from '@/components/Forms/Login';
 
 export default function Home() {
   return (
-    <div className=" flex justify-center items-center min-h-screen">
+    <div className=" flex justify-center items-center min-h-screen p-2.5">
       <LoginForm />
     </div>
   );
